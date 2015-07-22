460a566b1dabd48489cb96acfcb146c051468741
Brian Donohue <brian.donohue89@gmail.com>
2015-07-22 18:20:29 -0400

    Add ContentBlocker
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

---
+++ ContentBlocker/ActionRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ActionRequestHandler.h
+//  ContentBlocker
+//
+//  Created by Brian Donohue on 7/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
