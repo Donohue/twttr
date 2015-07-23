e6aa271ca92c887c0ebcd2b8d955d00492300fca
Brian Donohue <brian.donohue89@gmail.com>
2015-07-23 11:42:18 -0400

    Add tweets to core spotlight
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

--- twttr/Tweet.m
+++ twttr/Tweet.m
@@ -18,6 +18,17 @@
     return [df dateFromString:createdAt];
 }
 
+- (void)addToSpotlightSearch {
+    CSSearchableItemAttributeSet *attributes = [[CSSearchableItemAttributeSet alloc] initWithItemContentType:(NSString *)kUTTypeURL];
+    attributes.title = [NSString stringWithFormat:@"%@ @%@", self.displayname, self.username];
+    attributes.contentDescription = self.text;
+    
+    CSSearchableItem *item = [[CSSearchableItem alloc] initWithUniqueIdentifier:self.identifier
+                                                               domainIdentifier:@"com.bthdonohue.twttr.Tweet"
+                                                                   attributeSet:attributes];
+    [[CSSearchableIndex defaultSearchableIndex] indexSearchableItems:@[item] completionHandler:nil];
+}
+
 - (id)initWithDict:(NSDictionary *)dict {
     self = [super init];
     if (self) {
@@ -27,6 +38,8 @@
         self.displayname = dict[@"user"][@"name"];
         self.avatar_url = [NSURL URLWithString:dict[@"user"][@"profile_image_url"]];
         self.created = [Tweet dateFromCreatedAt:dict[@"created_at"]];
+        
+        [self addToSpotlightSearch];
     }
     return self;
 }
